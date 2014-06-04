26a203d3984a7f753768d3743245ceb728786fa7
moulinal <moulinal@pc-dg-039-02.iut2.upmf-grenoble.fr>
2014-06-04 14:30:37 +0200

    taxes

--- src/monopoly/Main.java
+++ src/monopoly/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// attention : bien pr�ciser tout le chemin d'acc�s au fichier case.data
-		Monopoly m = new Monopoly("/users/info/etu-s2/massonyo/Monopoly/Monopoly-master/src/monopoly/cases.data", "//users/info/etu-s2/massonyo/Monopoly/Monopoly-master/src/monopoly/cartes.data");
+		Monopoly m = new Monopoly("/users/info/etu-s2/moulinal/java/Monopoly/Monopoly/src/monopoly/cases.data", "/users/info/etu-s2/moulinal/java/Monopoly/Monopoly/src/monopoly/cartes.data");
                 while(!m.fini()){
                 m.Jouer();
                 }
